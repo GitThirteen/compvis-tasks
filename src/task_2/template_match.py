f2144be24a1a907786441a5789ad07beb15ed2d0
Michael <mich1.eickmeyer@gmail.com>
2022-04-06 14:39:25 +0100

    Changes to folder structure and task 2

--- src/task_2/template_match.py
+++ src/task_2/template_match.py
@@ -4,13 +4,14 @@ import sys
 import argparse
 import re
 import numpy as np
-import matplotlib.pyplot as plt
 import configparser as cfgp
 from helpers import draw_gaussian_pyramid, get_images
+from icecream import ic
 
 config = cfgp.ConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), '../settings.INI'))
 config = config['TASK2']
+
 '''
 TODO:
 Create a function to take in a list of scales and a gaussian pyramid of a template and return the template at those scales. - Michael
@@ -81,8 +82,7 @@ def create_gaussian_pyramid(image, rotations, scale_levels):
 def preprocess(pyramid):
     """
     Sets the background to 0 (black) for each scaled and rotated template.
-
-    ASSUMPTION: Assume template image itself does not get affected by this
+	We assume that the template image itself does not get affected by this.
 
     Parameters
     ----------
@@ -107,7 +107,7 @@ def preprocess(pyramid):
 
 def get_bbox_dims(img):
     """
-    Finds dimensions for bounding boxes in passed in image
+    Finds dimensions for bounding boxes in passed in image.
 
     Parameters
     ----------
@@ -137,6 +137,9 @@ def get_bbox_dims(img):
             dims = (w, h)
             cv2.rectangle(new,(x,y), (x+w,y+h), (255,0,0), 5)
             dims_list.append(dims)
+    
+    # cv2.imshow('bboxs detected from raw image',new)
+    # cv2.waitKey(0)
 
     return dims_list
 
@@ -145,8 +148,6 @@ def extract_templates_from_pyramid(pyramid, bboxes, option='closest'):
     """
     TODO Description
     Options: closest, upper, lower, both
-
-    CHECK IF RESAMPLING CAN FIX ISSUES
 
     Parameters:
     pyramid : dict
@@ -227,178 +228,251 @@ def extract_templates_from_pyramid(pyramid, bboxes, option='closest'):
     return filtered_pyramid
         
 
-def template_match(img, template_dict):
-    """
-    Convolves a set of templates across an image and returns a list containing the bbox of matches.
-    code based off tutorial : https://docs.opencv.org/3.4/d4/dc6/tutorial_py_template_matching.html
+def template_match(img, N,template_dict):
+	"""
+	Convolves a set of templates across an image and returns a list containing the bbox of matches.
+	code based off tutorial : https://docs.opencv.org/3.4/d4/dc6/tutorial_py_template_matching.html
 
-    Parameters
-    ----------
-    img : np.ndarray
-     return value of cv2.imread(img_path), a 3d numpy array
-
-    template_dict: dict
+	Parameters
+	----------
+	img : np.ndarray
+		return value of cv2.imread(img_path), a 3d numpy array
+	
+	N : int
+		number of objects found in test image
+	
+	template_dict: dict
 					key - class for template, eg 'theater, silo, flower ...'
-					value - list of templates for that class (varied resolution, rotation, scales etc)	
-     to be matched in image: [ndarray_temp_1, ndarray_temp_2,...]. Each template should be a 3D numpy array with using the same axis ordering conventions as used by arrays returned from cv2.imread()
-    
-    Returns
-    -------
-    bboxs_dict: dict
+					value - pyramid dict of rotated templates for that class (k: rotation_idx, v: list of scaled templates)	
+		Each template is 3D numpy array with using the same axis ordering conventions as used by arrays returned from cv2.imread()
+
+	Returns
+	-------
+	results_dict: dict
 					key - class of template 
 					value - list containing bbox corners for cv2.rectangles [upper_left, bottom_right] 
-					[[(x0,y0),(x1,y1)],...] if no match for a template, None tuple pairs returned for coordinates of bbox of that tuple. 
-    """
-    # TODO
+					
+	"""
+	# TODO
 				# IMPLEMENT DICTs
-    # test code
-    # Figure out way to pick the 'best' result from all the methods results. Define best ? Median bbox values?
-    # check how long for loop takes to loop over all templates, might wanna do a 3d convolution instead of several 2d convolves for performance
-    # - research what the difference between each method is
-    # figure out where the threshold is set for the response - will a 'match' always be found?
+	# test code
+	# Figure out way to pick the 'best' result from all the methods results. Define best ? Median bbox values?
+	# check how long for loop takes to loop over all templates, might wanna do a 3d convolution instead of several 2d convolves for performance
+	# - research what the difference between each method is
+	# figure out where the threshold is set for the response - will a 'match' always be found?
 
-    # EDGE CASES:
-    # no match found
-    # multiple matches found
+	# EDGE CASES:
+	# no match found
+	# multiple matches found
 
-    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+	# methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+			# 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+	methods = ['cv2.TM_SQDIFF']
+    # init results dict
+	class_bbox_scores_dict = {}
 
-    # init bbox array
-    bboxs_list = []
+    # loop over classes
+	for class_,template_pyramid in template_dict.items():
+		class_bboxes = []
+		# initialise score dict to keep track of template giving highest score
+		class_scores_dict = {}
 
-    # loop over templates
-    for template in template_dict:
-        # extract template dims
-        w, h = template.shape[::-1]
-        
-        # initialise lists to track bbox generated from each method
-        top_left_corners = np.zeros((len(methods),2))
-        bottom_right_corners = np.zeros((len(methods),2))
+		# loop over orientations
+		for template_idx, (rot_key, rotated_scaled_templates) in enumerate(template_pyramid.items()):
+			# loop over scaled templates
+			for template in rotated_scaled_templates:
+				# extract template dims
+				w, h = template.shape[0], template.shape[1]
+				# loop over similarity scores
+				for idx, meth in enumerate(methods):
+					img2 = img.copy()
+					method = eval(meth)
 
-        # loop over similarity scores
-        for idx, meth in enumerate(methods):
-            img2 = img.copy()
-            method = eval(meth)
-            
-            # Apply template Matching
-            res = cv2.matchTemplate(img2, template, method)
-            '''
-            EDGE CASE: multiple matches of template in image cv2.minMaxLoc will not be sufficient
-            how to detect if multiple detections?
-            '''
-            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res) 
-            
-            # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-            if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-                            top_left = np.asarraymin_loc
-            else:
-                            top_left = np.asarray(max_loc)
-            bottom_right = (top_left[0] + w, top_left[1] + h)
+					# Apply template Matching
+					res = cv2.matchTemplate(img2, template, method)
+					'''
+					EDGE CASE: multiple matches of template in image cv2.minMaxLoc will not be sufficient
+					how to detect if multiple detections?
+					'''
+					min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res) 
+					
+					# update score dict if current template has a better similarity / match
+					'''
+					make this block more efficient via a function to populate dict?
+					'''
+					if template_idx == 0:
+						# initialise class score dict if first iteration in template loop
+						if meth in ['cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']:
+							# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+							class_scores_dict[meth] = {'score':min_val}
+							top_left = np.asarray(min_loc)
+							bottom_right = np.asarray((top_left[0] + w, top_left[1] + h))
+							class_scores_dict[meth]['bbox']=(top_left,bottom_right)
 
-            # add corners to arrays
-            top_left_corners[idx] = top_left
-            bottom_right_corners[idx] = bottom_right
+						else:
+							class_scores_dict[meth] = {'score':min_val}
+							top_left = np.asarray(max_loc)
+							bottom_right = np.asarray((top_left[0] + w, top_left[1] + h))
+							class_scores_dict[meth]['bbox']=(top_left,bottom_right)
+					
+					else:
+						# only update dict if scores are better
+						if meth in ['cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']:
+							# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+							if class_scores_dict[meth]['score'] > min_val: 
+								class_scores_dict[meth]['score'] = min_val
+								top_left = np.asarray(min_loc)
+								bottom_right = np.asarray((top_left[0] + w, top_left[1] + h))
+								class_scores_dict[meth]['bbox']=(top_left,bottom_right)
+						else:
+							if class_scores_dict[meth]['score'] < max_val: 
+								class_scores_dict[meth]['score'] = max_val
+								top_left = np.asarray(max_loc)
+								bottom_right = np.asarray((top_left[0] + w, top_left[1] + h))
+								class_scores_dict[meth]['bbox']=(top_left,bottom_right)
+					
+					# ic(class_,class_scores_dict[meth]['score']/1e6)
+					# cv2.rectangle(img2,top_left,bottom_right,(255, 0, 0), 2)
+					# cv2.imshow('bbox found',img2)
+					# cv2.imshow('temp',template)
+					# cv2.waitKey(0)
+		# store final results for class in dict
+		class_bbox_scores_dict[class_] = class_scores_dict
+	# pick a N distinct bboxs, where N is number of objects found in image
+	classes = list(class_bbox_scores_dict.keys())
+	sorted_results = np.zeros((len(classes),len(methods)))
+	
+	# loop over methods
+	for idx,meth in enumerate(methods):
+		method_scores = []
+		# for each class look at the similarity scores given using that method
+		for class_, class_scores_dict in class_bbox_scores_dict.items():
+			method_scores.append((class_,class_scores_dict[meth]['score']/1e6))
+		# sort scores to find classes that were most strongly picked up by each method
+		sorted_method_scores = sorted(method_scores, key = lambda x: x[1]) 
+		# sort in descending order if meth TM_SQDIFF or TM_SQDIFF_NORMED else ascending
+		sorted_method_scores = sorted_method_scores[::-1] if meth in ["TM_SQDIFF", "TM_SQDIFF_NORMED"] else sorted_method_scores
+		sorted_results[:,idx]  = [classes.index(class_score_tuple[0]) for class_score_tuple in sorted_method_scores]
 
-            # draw bbox on img for debugging
-            if config.getboolean('ShowBbox') == True:
-                cv2.rectangle(img2,top_left, bottom_right, 255, 2)
+	
+	# pick top N classes
+	top_n_classes = []
+	for class_idx in np.median(sorted_results,1):
+		class_ = classes[int(class_idx)]
+		if class_ not in top_n_classes and len(top_n_classes)<N: top_n_classes.append(class_)
 
-        # find median corners - Introduce debugging print statements here, median taking could give poor results
-        '''
-        check edge case of no matchs handled properly at this step , should return [(none,none), (none,none)]
-        '''
-        median_top_left = np.median(top_left_corners,0)
-        median_bottom_right = np.median(bottom_right_corners,0)
+	# for each class find the median bbox coords
+	results_dict = {}
+	for class_ in top_n_classes:
+		class_scores_dict = class_bbox_scores_dict[class_]
+		top_left_corners = [class_scores_dict[meth]['bbox'][0] for meth in methods] 
+		bottom_right_corners = [class_scores_dict[meth]['bbox'][1] for meth in methods]
+		median_top_left = np.median(top_left_corners,0)
+		median_bottom_right = np.median(bottom_right_corners,0)
 
-        median_top_left = [None,None] if (median_bottom_right == 0).all() else median_bottom_right
-        median_bottom_right = [None,None] if (median_bottom_right == 0).all() else median_bottom_right
-
-        bboxs_list.append([median_top_left,median_bottom_right])
-
-    return bboxs_list
-
+		results_dict[class_] = (median_top_left,median_bottom_right)
+			
+	return results_dict
 
 
-def draw(img, bboxes):
+
+def draw(img, results_dict):
+	'''
+	Draws a bounding box around each detected object, including its class label.
+
+	Parameters
+	----------
+	img : np.ndarray
+					return value of cv2.imread(img_path), a 3d numpy array
+
+	bboxes : dictionary
+					dictionary with the image label as the key and the bbox top left and bottom right stored as tuples in a list as the value.
+					e.g. {"hydrant": [top_left, bottom_right], ...}
+
+	Returns
+	-------
+	final_image : np.ndarray
+					Final image with bboxes and labels represented as a 3d numpy array
     '''
-    Draws a bounding box around each detected object, including its class label.
+	
+	img2 = img.copy()
+	# pick a N distinct labels based on number of objects
+	ic(results_dict)
+	for label, bbox in results_dict.items():
+			cv2.rectangle(img2, ic(tuple(bbox[0].astype(int))), tuple(bbox[1].astype(int)), (255, 0, 0), 2)
+			cv2.putText(img2, label, (int(bbox[0][0]), int(bbox[1][1])+13), 0, 0.5, (255,0,0))
 
-    Parameters
-    ----------
-    img : np.ndarray
-        return value of cv2.imread(img_path), a 3d numpy array
+	cv2.imshow('test', img2)
+	cv2.waitKey(0)
+	
 
-    bboxes : dictionary
-        dictionary with the image label as the key and the bbox top left and bottom right stored as tuples in a list as the value.
-        e.g. {"hydrant": [top_left, bottom_right], ...}
 
-    Returns
-    -------
-    final_image : np.ndarray
-        Final image with bboxes and labels represented as a 3d numpy array
-    '''
-    result = img.copy()
-    for label, bbox in bboxes.items():
-        cv2.rectangle(result, bbox[0], bbox[1], (255, 0, 0), 2)
-        cv2.putText(result, label, (bbox[0][0], bbox[1][1]+13), 0, 0.5, (255,0,0))
+def algorithm_run(png_path):
+	loaded_templates = get_images(config.get('TrainingDataPath'))
+	test_image = cv2.imread(png_path)
+	rots = config.getint('PyramidRotations')
+	scale = config.getint('ScaleLevels')
 
-    plt.imshow(result)
-    plt.show()
-    return result
+	# Generates a pyramid for all templates
+	pyramids = [ ]
 
+	for template in loaded_templates:
+		pyramid = create_gaussian_pyramid(template, rots, scale)
+		pyramids.append(pyramid)
+
+	# Find bbox dims in training image
+	test_image_bboxes = get_bbox_dims(test_image)
+	N = len(test_image_bboxes)
+	# Extract image classes from file names
+	file_names = os.listdir(config.get('TrainingDataPath'))
+	class_names = [re.search('-(.*)\.', name).group(1) for name in file_names]
+    
+	templates = { }
+	i = 0
+	for pyramid in pyramids:
+		for _, scaled_img_list in pyramid.items():
+			for scaled_img in scaled_img_list:
+				_, thresh = cv2.threshold(cv2.cvtColor(scaled_img, cv2.COLOR_BGR2GRAY), 245, 255, cv2.THRESH_BINARY)
+				scaled_img[thresh == 255] = 0
+
+		class_name = class_names[i]
+		templates[class_name] = extract_templates_from_pyramid(pyramid, test_image_bboxes)
+		#extract_templates_from_pyramid(pyramids[0], bboxes, 'upper')
+		#extract_templates_from_pyramid(pyramids[0], bboxes, 'lower')
+		#extract_templates_from_pyramid(pyramids[0], bboxes, 'both')
+		i += 1
+
+	"""
+	Does this acc do anything?
+	"""
+	# Setting background to 0
+	transparent_test_image = test_image.copy()
+	_, thresh = cv2.threshold(cv2.cvtColor(transparent_test_image, cv2.COLOR_BGR2GRAY), 245, 255, cv2.THRESH_BINARY)
+	transparent_test_image[thresh == 255] = 0
+
+
+    # ic(loaded_templates[0].shape, test_image_bboxes)
+    
+    # # list of list containing templates resized to each scale detected in test_image_bbox [[temp_1_scale_1, temp_1_scale_2,...],...]
+    # scaled_templates = [[template.reshape(*dims,-1) for dims in test_image_bboxes] for template in loaded_templates]
+
+    # # test templates dict following k = class_name, v = dict(k: rot_key, v:list of loaded templates at different scales) 
+    # test_templates = {class_names[i]: {1:scaled_templates[i]} for i in range(class_names)}
+    # for k,v in templates.items():
+    #     temps = templates[k][1]
+    #     ic(len(temps), temps[0].shape)
+	final_bboxes_dict = template_match(transparent_test_image, N, templates)
+	# draw(test_image, final_bboxes_list)
+	return (test_image, final_bboxes_dict)
 
 def main():
-    # Parsing image file and declaring necessary params for pyramid generation
-    parser = argparse.ArgumentParser()
-    parser.add_argument("png_path", help="Path to a test image")
-    args = parser.parse_args()
+	# Parsing image file and declaring necessary params for pyramid generation
+	parser = argparse.ArgumentParser()
+	parser.add_argument("png_path", help="Path to a test image")
+	args = parser.parse_args()
 
-    templates = get_images(config.get('TrainingDataPath'))
-    test_image = cv2.imread(args.png_path)
-    rots = config.getint('PyramidRotations')
-    scale = config.getint('ScaleLevels')
-
-    # Generates a pyramid for all templates
-    pyramids = [ ]
-
-    for template in templates:
-        pyramid = create_gaussian_pyramid(template, rots, scale)
-        pyramids.append(pyramid)
-
-    # Find bbox dims in training image
-    test_image_bboxes = get_bbox_dims(test_image)
-
-    # Extract image classes from file names
-    file_names = os.listdir(config.get('TrainingDataPath'))
-    class_names = [re.search('-(.*)\.', name).group(1) for name in file_names]
-    
-    templates = { }
-    i = 0
-    for pyramid in pyramids:
-        for _, scaled_img_list in pyramid.items():
-            for scaled_img in scaled_img_list:
-                _, thresh = cv2.threshold(cv2.cvtColor(scaled_img, cv2.COLOR_BGR2GRAY), 245, 255, cv2.THRESH_BINARY)
-                scaled_img[thresh == 255] = 0
-
-        class_name = class_names[i]
-        templates[class_name] = extract_templates_from_pyramid(pyramid, test_image_bboxes)
-        #extract_templates_from_pyramid(pyramids[0], bboxes, 'upper')
-        #extract_templates_from_pyramid(pyramids[0], bboxes, 'lower')
-        #extract_templates_from_pyramid(pyramids[0], bboxes, 'both')
-        i += 1
-
-    # Adding transparency to test image background
-    transparent_test_image = test_image.copy()
-    _, thresh = cv2.threshold(cv2.cvtColor(transparent_test_image, cv2.COLOR_BGR2GRAY), 245, 255, cv2.THRESH_BINARY)
-    transparent_test_image[thresh == 255] = 0
-
-    for k, v in templates.items():
-        print(f'{k} = {type(v)}')
-
-    #final_bboxes_list = template_match(transparent_test_image, templates)
-    #draw(test_image, final_bboxes_list)
-
+	test_image, bboxes = algorithm_run(args.png_path)
+	draw(test_image, bboxes)
 
 if __name__ == "__main__":
     main()
--- src/task_2/test.py
+++ src/task_2/test.py
@@ -1,3 +1,70 @@
 import os
-from template_match import main as algorithm_run
+from template_match import algorithm_run
+import numpy as np
+import re
+import configparser as cfgp
 
+config = cfgp.ConfigParser()
+config.read(os.path.join(os.path.dirname(__file__), '../settings.INI'))
+config = config['TASK2']
+
+def parse_annotation_txt_files(path_to_txt_files):
+    '''
+    Parses directory of annotation text files with format:
+    class, bbox top left, bbox bottom right
+
+    Parameters
+    ----------
+    path_to_txt_files : str
+    path to directory containing annotation text files
+
+    Returns 
+    -------
+    labels_dict_list : list
+    list of dictionaries with k,v pairs of class labels : bbox corners
+    '''
+    txt_files = [f for f in os.listdir(path_to_txt_files) if f.endswith('.txt')]
+    labels_dict_list = []
+
+    for f in txt_files:
+        labels_dict = {}
+
+        with open(os.path.join(path_to_txt_files,f),'r') as f:
+            lines = [line.rstrip().split(',') for line in f.readlines()]
+
+            for line in lines:
+                class_ = line[0]
+                top_left = np.asarray(re.findall(r'\d+',','.join(line[1:3]))).astype(int)
+                bottom_right = np.asarray(re.findall(r'\d+',','.join(line[3:]))).astype(int)
+                labels_dict[class_] = (top_left, bottom_right)
+        labels_dict_list.append(labels_dict)
+
+    return labels_dict_list
+  
+labels_dir = config.get('AnnotationsPath')
+labels_dict_list = parse_annotation_txt_files(labels_dir)
+
+test_data_dir = config.get('TestImgDataPath')
+test_img_paths = [os.path.join(test_data_dir,f) for f in os.listdir(test_data_dir) if f.endswith('.png')]
+
+for idx,img_f in enumerate(test_img_paths):
+    _, results_dict = algorithm_run(img_f)
+    labels_dict = labels_dict_list[idx]
+
+    class_labels = set(labels_dict.keys())
+    class_segmented = set(results_dict.keys())
+    
+    if class_labels == class_segmented:
+        for class_ in class_labels:
+            segmented_bbox = results_dict[class_]
+            measured_bbox = labels_dict[class_]
+            # check if segmented bbox within 30 pixels of real one
+            top_left_diff = (segmented_bbox[0] - measured_bbox[0])**2
+            bottom_right_diff = (segmented_bbox[1] - measured_bbox[1])**2
+            total_diff = np.sum((top_left_diff + bottom_right_diff))**0.5
+
+            if total_diff>20:
+                print(f'BBOX for class: {class_} not a good match')
+        print(f'[SUCCESS] match for img: {img_f}')
+    else:
+        print(f'[ERROR] img_f: {img_f} failed, not all classes match')
