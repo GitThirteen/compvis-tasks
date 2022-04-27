Fixed error in get_bbox_iou, changed data output for the advanced data (added overlap, precision, recall)

src/task_2/algorithm.py
@@ -4,11 +4,12 @@ import sys
 import argparse
 import re
 import numpy as np
-from ..util import draw_gaussian_pyramid, get_images, get_bbox_dims, config, Algorithm, Logger
+from ..util import Algorithm, Logger, draw_gaussian_pyramid, get_images, get_bbox_dims, get_bbox_iou, config
 from icecream import ic
 
 cfg = config(Algorithm.TEMPLATE_MATCHING)
 LOGGER = Logger.get()
+
 
 def create_gaussian_pyramid(image, rotations, scale_levels):
     """
@@ -65,6 +66,22 @@ def create_gaussian_pyramid(image, rotations, scale_levels):
         draw_gaussian_pyramid(pyramid, scale_levels, rotations)
 
     return pyramid
+
+
+def generate_pyramids(training_data_path):
+    templates = get_images(training_data_path)
+
+    rots = cfg.getint('PyramidRotations')
+    scale = cfg.getint('ScaleLevels')
+
+    # Generates a pyramid for all templates
+    pyramids = [ ]
+
+    for template in templates:
+        pyramid = create_gaussian_pyramid(template, rots, scale)
+        pyramids.append(pyramid)
+
+    return pyramids
 
 
 def preprocess(pyramid):
@@ -229,7 +246,7 @@ def template_match(img, N, template_dict):
 
     # loop over classes
     for class_, template_pyramid in template_dict.items():
-        class_bboxes = []
+        # class_bboxes = []
         # initialise score dict to keep track of template giving highest score
         class_scores_dict = {}
 
@@ -287,7 +304,7 @@ def template_match(img, N, template_dict):
                                 bottom_right = np.asarray((top_left[0] + w, top_left[1] + h))
                                 class_scores_dict[meth]['bbox'] = (top_left, bottom_right)
 
-                # ic(class_,class_scores_dict[meth]['score']/1e6)
+                # LOGGER.DEBUG_IC(class_,class_scores_dict[meth]['score']/1e6)
                 # cv2.rectangle(img2,top_left,bottom_right,(255, 0, 0), 2)
                 # cv2.imshow('bbox found',img2)
                 # cv2.imshow('temp',template)
@@ -352,28 +369,6 @@ def template_match(img, N, template_dict):
     return results_dict
 
 
-def get_bbox_iou(bbox1, bbox2):
-    if (bbox1[0] == bbox2[0]).all() and (bbox1[1] == bbox2[1]).all():
-        return 1.0
-
-    x_left = max(bbox1[0][0], bbox2[0][0])
-    y_top = max(bbox1[0][1], bbox2[0][1])
-    x_right = min(bbox1[1][0], bbox2[1][0])
-    y_bottom = min(bbox1[1][1], bbox2[1][1])
-
-    if x_right < x_left or y_bottom < y_top:
-        return 0.0
-
-    intersection_area = (x_right - x_left + 1) * (y_bottom - y_top + 1)
-
-    bbox1_area = (bbox1[1][0] - bbox1[0][0] + 1) * (bbox1[1][1] - bbox1[1][0] + 1)
-    bbox2_area = (bbox2[1][0] - bbox2[0][0] + 1) * (bbox2[1][1] - bbox2[1][0] + 1)
-
-    iou = intersection_area / float(bbox1_area + bbox2_area - intersection_area)
-
-    return iou
-
-
 def draw(img, results_dict):
     """
 	Draws a bounding box around each detected object, including its class label.
@@ -408,6 +403,7 @@ def run(png_path, pyramids):
 
     # Find bbox dims in training image
     test_image_bboxes = get_bbox_dims(test_image)
+    LOGGER.DEBUG_IC(test_image_bboxes)
     N = len(test_image_bboxes)
     # Extract image classes from file names
     file_names = os.listdir(cfg.get('TrainingDataPath'))
@@ -436,22 +432,6 @@ def run(png_path, pyramids):
         draw(test_image, final_bboxes_dict)
 
     return final_bboxes_dict
-
-
-def generate_pyramids(training_data_path):
-    templates = get_images(training_data_path)
-
-    rots = cfg.getint('PyramidRotations')
-    scale = cfg.getint('ScaleLevels')
-
-    # Generates a pyramid for all templates
-    pyramids = [ ]
-
-    for template in templates:
-        pyramid = create_gaussian_pyramid(template, rots, scale)
-        pyramids.append(pyramid)
-
-    return pyramids
 
 
 def main():

tester.py
@@ -2,8 +2,9 @@ import sys
 import os
 import time
 import csv
+import cv2
 import numpy as np
-from src.util import Algorithm, Logger, config, sort_ascending, parse_annotation_txt_files
+from src.util import Algorithm, Logger, config, sort_ascending, parse_annotation_txt_files, get_bbox_iou
 from src.task_1 import algorithm as find_angle
 from src.task_2 import algorithm as template_match
 from src.task_3 import algorithm as sift
@@ -77,8 +78,8 @@ class Tester:
             path = self.img_path + '/' + name
             theta = find_angle.run(path)
 
-            angle = round(float(angle))
-            theta = round(float(theta))
+            angle = float(angle)
+            theta = round(theta, 2)
 
             LOGGER.INFO(name)
             LOGGER.INFO(f'Expected angle: {angle}°')
@@ -124,7 +125,7 @@ class Tester:
 
         if self.config.getboolean('PreciseData'):
             num_templates = len(templates)
-            header = ['Image', 'Runtime', 'SUCCESS/FAIL', 'False Positive Rate', 'True Positive Rate', 'Accuracy']
+            header = ['Image', 'Runtime', 'OUTCOME', 'Overlap', 'False Positive Rate', 'True Positive Rate', 'Precision', 'Recall']
             data = []
 
         start_time = time.time()
@@ -136,35 +137,49 @@ class Tester:
             class_labels = set(labels_dict.keys())
             class_segmented = set(results_dict.keys())
 
-            if self.config.getboolean('PreciseData'):
-                success = "FAIL"
+            overlaps = [ ]
 
-                negatives = num_templates-len(class_labels)
+            if self.config.getboolean('PreciseData'):
+                negatives = num_templates - len(class_labels)
                 true_positives = len([class_ for class_ in class_segmented if class_ in class_labels])
                 false_positives = len([class_ for class_ in class_segmented if class_ not in class_labels])
                 false_negatives = len([class_ for class_ in class_labels if class_ not in class_segmented])
-                true_negatives = negatives - false_negatives - false_positives
 
-                false_positive_rate = false_positives/negatives
-                true_positive_rate = true_positives/len(class_labels)
-                accuracy = ((true_positives+true_negatives)/(true_positives+true_negatives+false_positives+false_negatives))*100
+                fpr = (false_positives / negatives) * 100
+                tpr = (true_positives / len(class_labels)) * 100
+                precision = (true_positives / (true_positives + false_positives)) * 100
+                recall = (true_positives / (true_positives + false_negatives)) * 100
+
+                #accuracy = ((true_positives+true_negatives)/(true_positives+true_negatives+false_positives+false_negatives))*100
+            
+            #if class_labels == class_segmented:
+
+            for label in class_labels:
+                if (label not in results_dict) or (label not in labels_dict):
+                    # assuming 0 overlap if wrongly labeled or label not found
+                    overlaps.append(0)
+                    continue
+
+                segmented_bbox = results_dict[label]
+                measured_bbox = labels_dict[label]
+
+                overlap = get_bbox_iou(segmented_bbox, measured_bbox) * 100
+                overlaps.append(overlap)
+
+                # check if segmented bbox within 30 pixels of real one
+                top_left_diff = (segmented_bbox[0] - measured_bbox[0]) ** 2
+                bottom_right_diff = (segmented_bbox[1] - measured_bbox[1]) ** 2
+                total_diff = np.sum((top_left_diff + bottom_right_diff)) ** 0.5
+
+                if total_diff > 10:
+                    LOGGER.WARNING(f'BBOX for class: {label} not a good match')
 
             if class_labels == class_segmented:
-                for class_ in class_labels:
-                    segmented_bbox = results_dict[class_]
-                    measured_bbox = labels_dict[class_]
-                    # check if segmented bbox within 30 pixels of real one
-                    top_left_diff = (segmented_bbox[0] - measured_bbox[0]) ** 2
-                    bottom_right_diff = (segmented_bbox[1] - measured_bbox[1]) ** 2
-                    total_diff = np.sum((top_left_diff + bottom_right_diff)) ** 0.5
-
-                    if total_diff > 20:
-                        LOGGER.WARNING(f'BBOX for class: {class_} not a good match')
-
+                outcome = 'SUCCESS'
                 LOGGER.SUCCESS(f'match for img: {img_path}')
                 passes += 1
-                success = "SUCCESS"
             else:
+                outcome = 'FAIL'
                 LOGGER.ERROR(f'img_f: {img_path} failed, not all classes match')
                 fails += 1
 
@@ -172,8 +187,9 @@ class Tester:
             LOGGER.INFO(f'RUNTIME: {invid_end_time}s\n')
 
             if self.config.getboolean('PreciseData'):
-                LOGGER.INFO(f'TPR: {round(true_positive_rate*100, 2)}% | FPR: {round(false_positive_rate*100, 2)}% | Accuracy: {round(accuracy, 2)}%\n')
-                data.append([f'Image {i+1}', f'{invid_end_time}s', success, f'{round(false_positive_rate*100, 2)}%', f'{round(true_positive_rate*100, 2)}%', f'{round(accuracy, 2)}%'])
+                overlap = round(np.sum(overlaps) / len(overlaps), 2)
+                LOGGER.INFO(f'Overlap: {overlap}% | TPR: {tpr}% | FPR: {fpr}% | Precision: {precision}% | Recall: {recall}%\n')
+                data.append([f'Image {i+1}', f'{invid_end_time}s', outcome, f'{overlap}%', f'{fpr}%', f'{tpr}%', f'{precision}%', f'{recall}%'])
 
         end_time = round(time.time() - start_time, 3)
         if end_time > 60: